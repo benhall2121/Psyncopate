37f040ca494511b136653255362a4a87b03e6e3a
Ben Hall <benhall2121@yahoo.com>
2014-06-10 15:48:38 -0700

    benhall2121@yahoo custom deploy script

--- deploy.rb
+++ deploy.rb
@@ -1,23 +1,27 @@
 
-
-puts "RUBBUNG APP"
-
-
 require "rubygems"
 require 'net/ssh'
 require "net/scp"
 
-blacklist = [".", ".."]
+blacklist = [".", "..", ".DS_Store", ".git"]
 project_name = "psyncopate"
 user_name = "demo"
+server = "50.57.64.194"
+port = 30000
+file_path = "/home/#{user_name}/apps/#{project_name}"
+
+puts "
+
+*********************************************************************
+Connecting to #{server} with user #{user_name} on port #{port} ...
+*********************************************************************"
 
 
-Net::SSH.start("50.57.64.194", user_name, :port => 30000 ) do |ssh|
-  file_path = "/home/#{user_name}/apps/#{project_name}"
+Net::SSH.start(server, user_name, :port => port ) do |ssh|
 
   puts "deleting file #{file_path}"
   ssh.exec "rm -rf #{file_path}"
-  
+
   puts "creating file #{file_path}"
   ssh.exec "mkdir #{file_path}"
 
@@ -26,10 +30,15 @@ Net::SSH.start("50.57.64.194", user_name, :port => 30000 ) do |ssh|
       puts "Uploading #{file} to #{project_name}"
       ssh.scp.upload! file, "#{file_path}/#{file}"
     else
-      puts "The file: #{file} is on the #{project_name} blacklist"
+      puts "The file    #{file}     is on the #{project_name} BLACKLIST"
     end
   end
 end
+puts "*********************************************************************
+FINISHED!!!!!!!!!!!!!!!
+*********************************************************************
 
+
+"
 
 
